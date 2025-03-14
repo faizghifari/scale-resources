467211c7f016900f9172447fcd13c53657d90229
Faiz Ghifari Haznitrama <fgh1997@live.com>
2025-03-14 15:31:12 +0900

    update dedup script to dedup all id, cbn, ban

--- dedup.sh
+++ dedup.sh
@@ -1,9 +1,31 @@
 python -m text_dedup.minhash \
-   --path "dataset/paralel_2_lang/paralel_id_cbn_127k" \
+   --path "dataset/paralel_3_lang/combined_paralel_dataset_705k" \
    --local \
    --cache_dir "./cache" \
-   --output "dataset/paralel_2_lang/paralel_id_cbn_127k_dedup" \
-   --column "text" \
+   --output "dataset/paralel_3_lang/combined_paralel_dataset_705k_id" \
+   --column "indonesian" \
+   --batch_size 10000 \
+   --ngram 7 \
+   --threshold 0.8 \
+   --min_length 10 \
+
+python -m text_dedup.minhash \
+   --path "dataset/paralel_3_lang/combined_paralel_dataset_705k_id" \
+   --local \
+   --cache_dir "./cache" \
+   --output "dataset/paralel_3_lang/combined_paralel_dataset_705k_id_cbn" \
+   --column "cirebonese" \
+   --batch_size 10000 \
+   --ngram 7 \
+   --threshold 0.8 \
+   --min_length 10 \
+
+python -m text_dedup.minhash \
+   --path "dataset/paralel_3_lang/combined_paralel_dataset_705k_id_cbn" \
+   --local \
+   --cache_dir "./cache" \
+   --output "dataset/paralel_3_lang/combined_paralel_dataset_705k_dedup" \
+   --column "balinese" \
    --batch_size 10000 \
    --ngram 7 \
    --threshold 0.8 \
