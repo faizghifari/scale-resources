3639f98bb2141b92a096426d3065db2d54353412
Faiz Ghifari Haznitrama <fgh1997@live.com>
2025-03-19 15:31:59 +0900

    update dedup.sh

--- dedup.sh
+++ dedup.sh
@@ -1,32 +1,10 @@
 python -m text_dedup.minhash \
-   --path "dataset/paralel_3_lang/combined_paralel_dataset_705k" \
+   --path "dataset/bali_hq/all_bali_hq" \
    --local \
    --cache_dir "./cache" \
-   --output "dataset/paralel_3_lang/combined_paralel_dataset_705k_id" \
-   --column "indonesian" \
+   --output "dataset/bali_hq/all_bali_hq_dedup" \
+   --column "text" \
    --batch_size 10000 \
    --ngram 7 \
    --threshold 0.8 \
-   --min_length 10 \
-
-python -m text_dedup.minhash \
-   --path "dataset/paralel_3_lang/combined_paralel_dataset_705k_id" \
-   --local \
-   --cache_dir "./cache" \
-   --output "dataset/paralel_3_lang/combined_paralel_dataset_705k_id_cbn" \
-   --column "cirebonese" \
-   --batch_size 10000 \
-   --ngram 7 \
-   --threshold 0.8 \
-   --min_length 10 \
-
-python -m text_dedup.minhash \
-   --path "dataset/paralel_3_lang/combined_paralel_dataset_705k_id_cbn" \
-   --local \
-   --cache_dir "./cache" \
-   --output "dataset/paralel_3_lang/combined_paralel_dataset_705k_dedup" \
-   --column "balinese" \
-   --batch_size 10000 \
-   --ngram 7 \
-   --threshold 0.8 \
-   --min_length 10 \
+   --min_length 5 \
